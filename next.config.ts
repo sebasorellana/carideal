8996ad9dd371851e19f688c047089d8f24e8a22b
iamivansantana <iam.ivansantana@gmail.com>
2026-08-19 19:48:40 -0600

    Configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
+
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	output: 'export',
+
+	basePath: isProd ? '/carideal' : '',
+	assetPrefix: isProd ? '/carideal/' : '',
+
+	images: {
+		unoptimized: true,
+	},
+
+	trailingSlash: true,
 };
 
 export default nextConfig;
